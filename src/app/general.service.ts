05e93e58f06f59bb1d5870ab7dc7c528bbe35c43
spappa3k <pappalardo.dev@gmail.com>
2024-12-04 18:55:03 +0100

    .

---
+++ src/app/general.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { dataModel } from './models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeneralService {
+
+  constructor(private http:HttpClient) { }
+
+
+  getData():Observable<dataModel[]>{
+return this.http.get<dataModel[]>("http://localhost:3000/posts")
+
+  }
+}
---
+++ src/app/models/models.ts
@@ -0,0 +1,6 @@
+export interface dataModel {
+  userId: number
+  id: string
+  title: string
+  body: string
+}
